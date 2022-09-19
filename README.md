f186b1aec63b62e9ab65fa526b9d6d95fca58c99
Pankeeva Olga <0lga.pankeeva@mail.ru>
2022-09-19 09:20:29 +0400

    commit add

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/x1n8raaugkq4s3d7?svg=true)](https://ci.appveyor.com/project/0lgaPankeeva/java2-2-selenide)
---
+++ src/test/java/ru/netology/CardTest.java
@@ -0,0 +1,31 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CardTest {
+
+    @Test
+    void test() {
+
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[placeholder=\"Город\"]").setValue("Самара");
+        $("[placeholder=\"Дата встречи\"]").doubleClick().sendKeys(Keys.DELETE);
+        $("[placeholder=\"Дата встречи\"]").setValue("22.09.2022");
+        $("[data-test-id=\"name\"] input").setValue("Иванов Иван");
+        $("[data-test-id=\"phone\"] input").setValue("+12345678900");
+        $x("//span[contains(text(), 'Я соглашаюсь с условиями обработки')]").click();
+        $(".button__content").click();
+        $x("//div[text()= 'Успешно!']").should(Condition.visible, Duration.ofSeconds(15));
+        $x("//div[contains(text(), 'Встреча успешно забронирована')]").should(Condition.visible, Duration.ofSeconds(15));
+
+    }
+}
